fbce7f6252891d07e351536f9daeaddc9a1c0cb2
vighnesh <vighneshbheed@outlook.com>
2022-10-26 10:40:13 +0200

    marker popup content component

---
+++ src/components/MapPopup.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import type {Feature} from 'geojson';
+    import {format_time} from "../lib/utils.js";
+
+    export let location: Feature;
+
+</script>
+
+
+<div style="width:100%;text-align:center;font-weight:600">
+    <h1 class="f-title">{location.properties.type}</h1>
+    <hr class="solid">
+    <h3>Location: {location.properties.place}</h3>
+    <h4>Magnitude: {location.properties.mag}</h4>
+
+    <p>{format_time(location.properties.time)}</p>
+    <a href=location.properties.url>Details</a>
+
+</div>
+
+<style>
+    hr.solid {
+        border-top: 3px solid #bbb;
+    }
+
+    .f-title {
+        text-transform: capitalize;
+    }
+</style>
